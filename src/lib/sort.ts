1f816c4fd91d4db12f2d14c35cb4140b32c195d2
Daniel Sieradski <daniel@self.agency>
2024-02-06 16:46:48 -0500

    reworkip
    Signed-off-by: Daniel Sieradski <daniel@self.agency>

--- src/lib/sort.ts
+++ src/lib/sort.ts
@@ -17,43 +17,63 @@ async function getPubs() {
 
 async function sortFiles() {
   // const pubs = await getPubs();
+
+  // set base folder
   const base = `${process.cwd()}/src/content`;
-  const files = (await readdir(base, { withFileTypes: true }))
+  const unsorted = `${base}/unsorted`;
+  const sorted = `${base}/sorted`;
+
+  // get all files in the base folder
+  const files = (await readdir(unsorted, { withFileTypes: true }))
     .filter(dirent => dirent.isFile() && !dirent.name.endsWith('.json'))
     .map(dirent => dirent.name);
 
+  // loop through each file
   for (const file of files) {
     console.log(file);
-    let content = await readFile(`${base}/${file}`, 'utf-8');
 
+    // read the file content
+    let content = await readFile(`${unsorted}/${file}`, 'utf-8');
+
+    // get the notes
     let note = content.match(/^notes: (.+)\npublication/m);
 
+    // if notes exist, replace the double quotes with single quotes and remove new lines
     if (note && note[1]) {
       let newNote = note[1].replace(/"/g, "'").replace(/\n/g, ' ');
       content = content.replace(/^notes: Source: (.+)$/gm, `source: $1`);
       content = content.replace(/^notes:.+?\[(.+)\].+?$/gm, `notes: $1`);
     }
 
+    // get the blurb
     let blurb = content.match(/^blurb: (.+)$/gm);
+
+    // if blurb exists and it doesn't contain double quotes, add them
     if (blurb && !blurb[0]?.includes('"')) {
       content = content.replace(/^blurb: (.+)\nnotes/gm, `blurb: "$1"\nnotes`);
     }
 
+    // parse the frontmatter
     const meta = matter(content);
 
+    // get the categories
     const categories = meta.data.categories?.map((cat: string) =>
       cat?.replace(/_categories\//, '').replace(/\.md/, '')
     );
 
+    // get the publication
     const publication = meta.data.publication?.replace(/_publications\//, '').replace(/\.md/, '');
 
+    // get the title and format it
     const title = meta.data.title
       .toLowerCase()
       .replace(/ /g, '_')
       .replace(/[^a-zA-Z0-9-]/g, '_');
 
+    // get the date and format it
     const date = dayjs(meta.data.date).format('YYYY-MM-DD');
 
+    // create the new file content
     const newContent = `---
 title: ${meta.data.title.replace(/"/g, '')}
 subtitle: ${meta.data.subtitle ?? ''}
@@ -66,23 +86,27 @@ publication: ${publication ? publication.replace(/-/g, '_') : ''}
 ${meta.content}
 `;
 
-    const newPath =
-      categories?.length > 0
-        ? `${base}/sorted/${categories[0]}/${date}_${title}.md`
-        : `${base}/sorted/${date}_${title}.md`;
+    // create the new file path
+    let newPath;
 
-    if (categories) {
-      for (const cat of categories) {
-        if (!existsSync(`${base}/sorted/${cat}`)) {
-          await mkdir(`${base}/sorted/${cat}`);
-        }
+    // if categories exist, create a folder for the first category and save the file there
+    if (categories?.length > 0) {
+      newPath = `${sorted}/${categories[0]}/${date}_${title}.md`;
+
+      if (!existsSync(`${sorted}/${categories[0]}`)) {
+        await mkdir(`${sorted}/${categories[0]}`);
       }
+      // if no categories exist, save the file in the base folder
+    } else {
+      newPath = `${sorted}/${date}_${title}.md`;
     }
 
+    // write the new file
     await writeFile(newPath, newContent);
   }
 }
 
+// run the function
 try {
   sortFiles();
 } catch (e) {
